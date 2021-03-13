bd02f4aced79e37baeae05f3885e4ee8a742eef4
misheckgithongo <misheckgithongo@gmail.com>
2021-03-13 14:34:15 +0300

    alert system

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    extends: 'airbnb',
+    rules: {
+      'linebreak-style': 'off',
+      semi: ['error', 'never'],
+      quotes: ['error', 'single'],
+      'no-unused-vars': 'warn',
+      'func-names': 'off',
+      'object-shorthand': 'off',
+      'no-undef': 'off',
+    },
+  }
+  
